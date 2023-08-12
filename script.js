7181dc891019021c55f02c5d9ccb01936e402be5
Yuvraj <oberoiyuvraj024@gmail.com>
2023-08-12 17:36:41 +0530

    Add the complete logic for 5 round game

--- script.js
+++ script.js
@@ -11,7 +11,10 @@ function getComputerChoice(){
     }
 }
 
-function playRound(playerSelection,computerSelection){
+function playRound(){
+    const player=prompt("Enter your choice (Rock,Paper or Scissor?");
+    const computerSelection=getComputerChoice();
+    const playerSelection=player.toLowerCase();
     console.log("User: "+ playerSelection);
     console.log("Computer:"+computerSelection);
     if(playerSelection==computerSelection){
@@ -34,11 +37,35 @@ function playRound(playerSelection,computerSelection){
         }return "You Win! Scissors beats Paper";
     }
     else{
-         console.log("Enter a valid Choice!");
+         return "Enter a right choice";
     }    
 }
 
-const playerSelection=prompt("Enter your choice (Rock,Paper or Scissor?");
-const computerSelection=getComputerChoice();
+function game(){
+    let userPoints=0,computerPoints=0;
+    for (let i = 0; i < 5; i++) {
+        console.log("ROUND " +(i+1));
+        const ans=playRound();
+        console.log(ans);
+        // console.log(playRound());
+        if(ans.includes("Win")){
+            userPoints+=1;
+        }
+        else if(ans.includes("Lose")){
+            computerPoints+=1;
+        }
+        console.log("User :"+userPoints);
+        console.log("Computer: "+computerPoints);
+
+        
+    }
+    if(userPoints>computerPoints){
+        console.log("user win by "+ userPoints +":"+computerPoints);
+    }else{
+        console.log("computer win by "+ userPoints + ":"+computerPoints);
+    }
+}
+game();
+
 // console.log(getComputerChoice());
-console.log(playRound(playerSelection.toLowerCase(),computerSelection));
+// console.log(playRound(playerSelection.toLowerCase(),computerSelection));
