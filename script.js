28ee5039b1012fd37c468254199a9ef60459e0b2
Yuvraj <oberoiyuvraj024@gmail.com>
2023-08-14 17:38:47 +0530

    Added basic ui for the game

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const btnRock=document.querySelector(".rock");
+const btnPaper=document.querySelector(".paper");
+const btnScissors=document.querySelector(".scissors");
+const container=document.querySelector(".container");
+container.addEventListener("click",playRound);
+
 function getComputerChoice(){
     const randomNo=Math.random();
     if(randomNo<0.33){
@@ -11,12 +17,22 @@ function getComputerChoice(){
     }
 }
 
-function playRound(){
-    const player=prompt("Enter your choice (Rock,Paper or Scissor?");
-    const computerSelection=getComputerChoice();
-    const playerSelection=player.toLowerCase();
-    console.log("User: "+ playerSelection);
-    console.log("Computer:"+computerSelection);
+function playRound(e){
+    // const player=prompt("Enter your choice (Rock,Paper or Scissor?");
+    let playerSelection;
+    if(e.target.classList.contains("rock")){
+        playerSelection="rock";
+    }
+    else if(e.target.classList.contains("paper")){
+        playerSelection="paper";
+    }
+    else if(e.target.classList.contains("scissors")){
+        playerSelection="scissors";
+    }
+    const computerSelection=getComputerChoice(); 
+    
+    // console.log("User: "+ playerSelection);
+    // console.log("Computer:"+computerSelection);
     if(playerSelection==computerSelection){
         return "It's a tie";
     }
@@ -41,31 +57,28 @@ function playRound(){
     }    
 }
 
-function game(){
-    let userPoints=0,computerPoints=0;
-    for (let i = 0; i < 5; i++) {
-        console.log("ROUND " +(i+1));
-        const ans=playRound();
-        console.log(ans);
-        // console.log(playRound());
-        if(ans.includes("Win")){
-            userPoints+=1;
-        }
-        else if(ans.includes("Lose")){
-            computerPoints+=1;
-        }
-        console.log("User :"+userPoints);
-        console.log("Computer: "+computerPoints);
+// function game(){
+//     let userPoints=0,computerPoints=0;
+//     for (let i = 0; i < 5; i++) {
+//         console.log("ROUND " +(i+1));
+//         const ans=playRound();
+//         console.log(ans);
+//         // console.log(playRound());
+//         if(ans.includes("Win")){
+//             userPoints+=1;
+//         }
+//         else if(ans.includes("Lose")){
+//             computerPoints+=1;
+//         }
+//         console.log("User :"+userPoints);
+//         console.log("Computer: "+computerPoints);
 
         
-    }
-    if(userPoints>computerPoints){
-        console.log("user win by "+ userPoints +":"+computerPoints);
-    }else{
-        console.log("computer win by "+ userPoints + ":"+computerPoints);
-    }
-}
-game();
-
-// console.log(getComputerChoice());
-// console.log(playRound(playerSelection.toLowerCase(),computerSelection));
+//     }
+//     if(userPoints>computerPoints){
+//         console.log("user win by "+ userPoints +":"+computerPoints);
+//     }else{
+//         console.log("computer win by "+ userPoints + ":"+computerPoints);
+//     }
+// }
+// game();
